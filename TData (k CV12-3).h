dfc3eacba629d66d7c5402343fc28ef62ff7c585
Petr Oblouk <60695220+peoblouk@users.noreply.github.com>
2023-12-05 20:52:54 +0100

    cv12

---
+++ TData (k CV12-3).h
@@ -0,0 +1,22 @@
+﻿#ifndef TDATA_H
+#define TDATA_H
+
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include "check.h"
+
+struct TData
+{
+	double* iPtr;
+	size_t iNum;
+};
+
+int data_read(FILE* aFile, struct TData* aPtrData); // Funkce na �ten� vstupn�ch dat
+int data_write(FILE* aFile, const struct TData aData); // Funkce na z�pis
+
+int data_minmax(const struct TData aData, double* aPtrMin, double* aPtrMax);
+int data_minmax_write(FILE* aFile, double aMin, double aMax);
+void data_destroy(struct TData* aPtrData);
+
+#endif // TDATA_h
